623549b7b19617fa12cff62606767a6d36757845
boss_hoff <rhoffman0214@gmail.com>
2022-01-31 21:29:48 -0600

    working on adding bindings to textbox (#1)

---
+++ src/BobsComponent.Library/Constants/TextBoxType.cs
@@ -0,0 +1,9 @@
+namespace BobsComponent.Library.Constants
+{
+    public sealed class TextBoxType
+    {
+        public string Text { get { return "text"; } }
+        public string Password { get { return "password"; } }
+        public string Email { get { return "email"; } }
+    }
+}
