fd4b9b6bd30c69a82bdac21952c3e27b52607f5b
boss_hoff <rhoffman0214@gmail.com>
2022-01-31 21:31:10 -0600

    Revert "working on adding bindings to textbox (#1)" (#2)
    This reverts commit 623549b7b19617fa12cff62606767a6d36757845.

--- src/BobsComponent.Library/Constants/TextBoxType.cs
+++
@@ -1,9 +0,0 @@
-namespace BobsComponent.Library.Constants
-{
-    public sealed class TextBoxType
-    {
-        public string Text { get { return "text"; } }
-        public string Password { get { return "password"; } }
-        public string Email { get { return "email"; } }
-    }
-}
